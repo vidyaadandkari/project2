73fdc7bb3c79dedcba665fa23f9c02bd13db712d
vidyaadandkari <dandkarividya2007@gmail.com>
2026-06-01 16:55:23 +0400

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld {
   public static void main(String[] args)  {
     System.out.println("omggggg helloworld");
-    system.out.println("VIDYA NITIN DANDKARI"):
+    System.out.println("VIDYA NITIN DANDKARI"):
   }
 }
